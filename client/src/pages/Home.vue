bb3e5ed5aa7c3ae15764c50d237b5b83eecd1578
CodingSeo <seojingyo@hotmail.com>
2020-07-15 12:36:36 +0900

    Changed some CC

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -7,16 +7,57 @@
         <v-flex>
           <v-card class="my-3" height="30%">
             <v-card-text>
-              <div>{{this.RandomPost.id}}</div>
-              <p class="display-1 text--primary">{{this.RandomPost.title}}</p>
-              <div class="text--primary">{{this.RandomPost.body}}</div>
+              <v-container>
+                <v-layout row wrap>
+                  <template v-if="this.RandomPostImages.length > 0">
+                    <img
+                      :src="RandomPostImages[0].src"
+                      class="mx-10"
+                      style="width:200px;height:200px;margin-left:auto;"
+                    />
+                  </template>
+                  <template v-else>
+                    <img
+                      src="@/images/samplePic.png"
+                      class="mx-10"
+                      style="width:200px;height:200px;margin-left:auto;"
+                    />
+                  </template>
+
+                  <v-flex col-xs-12>
+                    <v-flex row-xs-8>
+                      <div>{{ this.RandomPost.id }}</div>
+                    </v-flex>
+                    <v-flex row-xs-8>
+                      <p class="display-1 text--primary">
+                        {{ this.RandomPost.title }}
+                      </p>
+                    </v-flex>
+                    <v-flex row-xs-8>
+                      <div class="text--primary">
+                        {{ this.RandomPost.body }}
+                      </div>
+                    </v-flex>
+                  </v-flex>
+                </v-layout>
+                <router-link
+                  class="routerLink"
+                  :to="`/${this.RandomPostCategory}/post/1`"
+                >
+                  <v-btn text>
+                    <span>To Page</span>
+                  </v-btn>
+                </router-link>
+              </v-container>
             </v-card-text>
           </v-card>
 
           <v-divider></v-divider>
           <v-card class="my-3 indigo">
             <v-card-title>
-              <p class="display-1 pl-4 pt-2 text-uppercase white--text">Most Viewed Posts</p>
+              <p class="display-1 pl-4 pt-2 text-uppercase white--text">
+                Most Viewed Posts
+              </p>
             </v-card-title>
             <v-divider></v-divider>
             <v-carousel
@@ -27,9 +68,16 @@
               hide-delimiters
               show-arrows-on-hover
             >
-              <v-carousel-item v-for="post in this.MostViewedPosts" :key="post.id">
+              <v-carousel-item
+                v-for="post in this.MostViewedPosts"
+                :key="post.id"
+              >
                 <v-sheet height="100%">
-                  <v-row class="pa-10 fill-height" align="center" justify="center">
+                  <v-row
+                    class="pa-10 fill-height"
+                    align="center"
+                    justify="center"
+                  >
                     <v-flex xs12 md6>
                       <div class="caption grey--text">postName</div>
                       <div>{{ post.title }}</div>
@@ -55,7 +103,9 @@
 
           <v-card class="my-3 indigo">
             <v-card-title class="height=20">
-              <p class="display-1 pl-4 pt-2 text-uppercase white--text">Most Recent Posts</p>
+              <p class="display-1 pl-4 pt-2 text-uppercase white--text">
+                Most Recent Posts
+              </p>
             </v-card-title>
             <v-divider></v-divider>
             <v-carousel
@@ -66,9 +116,16 @@
               hide-delimiters
               show-arrows-on-hover
             >
-              <v-carousel-item v-for="post in this.MostRecentPosts" :key="post.id">
+              <v-carousel-item
+                v-for="post in this.MostRecentPosts"
+                :key="post.id"
+              >
                 <v-sheet height="100%">
-                  <v-row class="pa-10 fill-height" align="center" justify="center">
+                  <v-row
+                    class="pa-10 fill-height"
+                    align="center"
+                    justify="center"
+                  >
                     <v-flex xs12 md6>
                       <div class="caption grey--text">postName</div>
                       <div>{{ post.title }}</div>
@@ -105,6 +162,8 @@ export default {
     return {
       MostViewedPosts: [],
       MostRecentPosts: [],
+      RandomPostImages: [],
+      RandomPostCategory:'',
       RandomPost: new Post()
     };
   },
@@ -114,7 +173,9 @@ export default {
       this.RandomPost.id = content.user_id;
       this.RandomPost.body = content.content.body;
       this.RandomPost.title = content.title;
-      console.log(this.RandomPost);
+      this.RandomPostCategory = content.title;
+      this.RandomPostImages = content.content.images;
+      console.log(content);
     });
     this.$http.get(`api/posts/mostViews/5`).then(result => {
       let content = result.data;
